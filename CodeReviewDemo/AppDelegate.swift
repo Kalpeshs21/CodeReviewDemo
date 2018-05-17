72180bd7b8901662613ff66c6e8682771803e8d5
Kalpesh Surve <kalpeshsurve21@outlook.com>
2018-05-17 11:07:31 +0530

    First pull request

--- CodeReviewDemo/AppDelegate.swift
+++ CodeReviewDemo/AppDelegate.swift
@@ -16,7 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        
+        //This is the first pull request
+        
         return true
+    }
+    
+    func appDel() {
+        
+        
+        
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- CodeReviewDemo/ViewController.swift
+++ CodeReviewDemo/ViewController.swift
@@ -12,12 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //This is the first pull request
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func myTestFunc() {
+        
+        
+        
+        //here im going to write some code
     }
 
 
