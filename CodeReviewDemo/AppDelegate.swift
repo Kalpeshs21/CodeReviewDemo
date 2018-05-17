e8c236db2f359b0cee48000abee56d1aa2747f1e
Kalpesh Surve <kalpeshsurve21@outlook.com>
2018-05-17 14:41:09 +0530

    Again duplicate

--- CodeReviewDemo/AppDelegate.swift
+++ CodeReviewDemo/AppDelegate.swift
@@ -45,6 +45,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
     }
+    
+    
+    func appDel() {
+        
+        var a=0
+        var b=1
+        
+        for i in 0...1000 {
+            if a==b {
+                print("True")
+            }
+            a+=1
+        }
+        
+        
+        var a=0
+        var b=1
+        
+        for i in 0...1000 {
+            if a==b {
+                print("True")
+            }
+            a+=1
+        }
+        
+        
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
